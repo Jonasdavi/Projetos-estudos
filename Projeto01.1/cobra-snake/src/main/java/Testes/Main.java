009a70ad45faa4362efb18f82586de40505c67d6
Jonasdavi <jonas.davi@escolar.ifrn.edu.br>
2025-01-02 02:07:17 -0300

    cardLayout

---
+++ Projeto01.1/cobra-snake/src/main/java/Testes/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Testes;
+
+/**
+ *
+ * @author Jonas
+ */
+public class Main {
+    public static void main(String[] args) {
+        Tela t= new Tela();
+        
+        
+        t.setSize(600, 300);
+        t.setVisible(true);
+    }
+}
