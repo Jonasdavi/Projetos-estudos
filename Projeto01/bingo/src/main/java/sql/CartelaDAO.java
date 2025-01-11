a944e2ad033afb061e633d3c61c40a42e52ffaee
Gerson Everton <everton4814@gmail.com>
2025-01-10 21:24:01 -0300

    Correção de um erro

--- Projeto01/bingo/src/main/java/sql/CartelaDAO.java
+++ Projeto01/bingo/src/main/java/sql/CartelaDAO.java
@@ -114,7 +114,7 @@ public class CartelaDAO
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < matriz.length; i++)
         {
-            for (int j = 0; i < matriz[i].length; i++)
+            for (int j = 0; j < matriz[i].length; j++)
             {
                 sb.append(matriz[i][j]).append(",");
             }
