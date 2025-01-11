32a2d4a3a7eb8ea90511c84ccf6f5147fffd8df7
Gerson Everton <everton4814@gmail.com>
2025-01-11 19:59:49 -0300

    Banco de dados ligado com a nuvem

--- Projeto01/bingo/src/main/java/sql/Conexao.java
+++ Projeto01/bingo/src/main/java/sql/Conexao.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class Conexao {
     
-    private static final String URL = "jdbc:mysql://localhost:3306/bingo";
-    private static final String USUARIO = "root";
-    private static final String SENHA = "";
+    private static final String URL = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10757136";
+    private static final String USUARIO = "sql10757136";
+    private static final String SENHA = "emJRsUEpVz";
 
     public static Connection obterConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
