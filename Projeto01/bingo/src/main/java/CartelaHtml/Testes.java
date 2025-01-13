26c869434150dc1c22102378ae9beaf835067f8c
Darlan-V <wvinicius951@gmail.com>
2025-01-13 15:44:27 -0300

    Update Testes.java

--- Projeto01/bingo/src/main/java/CartelaHtml/Testes.java
+++ Projeto01/bingo/src/main/java/CartelaHtml/Testes.java
@@ -36,19 +36,8 @@ public class Testes {
 
 
 
-        //bingo.venderCartela(new Jogador("Jogador1", 1, bingo)); // Supondo que isso crie a cartela para o jogador1
-        /*Document doc;
-        File file = new File("src/main/java/cartelahtml/index.html");
-        doc = Jsoup.parse(file, "UTF-8");*/
-        //ManipularCart manipularCart = new ManipularCart();
-        //manipularCart.adicionarCartela(bingo.pegarCartelaPeloId(1));
 
-        // Se quiser adicionar mais cartelas, continue o processo
-        //bingo.venderCartela(new Jogador("Jogador2", 2, bingo));
-        //manipularCart.adicionarCartela(bingo.pegarCartelaPeloId(2));
-
-        // Apagar cartelas
-        //manipularCart.apagarCartelas();
+        
     }
 }
 
